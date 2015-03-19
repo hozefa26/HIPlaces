8bdabff58561ace4cbfc263265bfc861d73c21eb
Hozefa Indorewala <me@hozefaindorewala.com>
2015-03-19 17:44:03 +0100

    Added TableViewController for PlaceDetails

---
+++ HIPlaces/HIPlacesExample/HIPlaceDetailsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  HIPlaceDetailsTableViewController.h
+//  HIPlaces
+//
+//  Created by Hozefa Indorewala on 19/03/15.
+//  Copyright (c) 2015 Hozefa Indorewala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HIPlaceDetailsTableViewController : UITableViewController
+
+@end
