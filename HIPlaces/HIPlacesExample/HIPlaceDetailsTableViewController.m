fdc604343ddbc4874def82e0b932c6c69d61e94c
Hozefa Indorewala <me@hozefaindorewala.com>
2015-04-06 17:19:30 +0300

    Improved error messages in alertview for example

--- HIPlaces/HIPlacesExample/HIPlaceDetailsTableViewController.m
+++ HIPlaces/HIPlacesExample/HIPlaceDetailsTableViewController.m
@@ -114,9 +114,46 @@
 
 - (void)placesManager:(HIPlacesManager *)placesManager searchForPlaceDetailsResultDidFailWithError:(NSError *)error
 {
-    NSString *errorCode = [NSString stringWithFormat:@"Error Code: %ld", (long)error.code];
+    NSString *alertViewTitle;
+    switch (error.code) {
+        case HIPlacesErrorZeroResults:
+            alertViewTitle = @"No results found!";
+            break;
+            
+        case HIPlacesErrorOverQueryLimit:
+            alertViewTitle = @"Over query limit!";
+            break;
+            
+        case HIPlacesErrorRequestDenied:
+            alertViewTitle = @"Request denied!";
+            break;
+            
+        case HIPlacesErrorInvalidRequest:
+            alertViewTitle = @"Invalid request!";
+            break;
+            
+        case HIPlacesErrorNotFound:
+            alertViewTitle = @"Not found!";
+            break;
+            
+        case HIPlacesErrorUnkownError:
+            alertViewTitle = @"Unknown error!";
+            break;
+            
+        case HIPlacesErrorInvalidJSON:
+            alertViewTitle = @"Invalid JSON!";
+            break;
+            
+        case HIPlacesErrorConnectionFailed:
+            alertViewTitle = @"Connection failed!";
+            break;
+            
+        default:
+            break;
+    }
+    
     UIAlertView *alertView = [[UIAlertView alloc]
-                              initWithTitle:errorCode
+                              initWithTitle:alertViewTitle
                               message:nil
                               delegate:nil
                               cancelButtonTitle:@"OK"
